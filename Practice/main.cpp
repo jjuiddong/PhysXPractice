498ccbbdfecd62b680e74e4f35ba948c1cca29ba
jjuiddong <jjuiddong@gmail.com>
2013-11-10 18:23:37 +0900

    compiler success practice project

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -11,9 +11,83 @@
 #include "PhysXSampleApplication.h"
 #include "PsFile.h"
 
+using namespace SampleFramework;
 
-int _tmain(int argc, _TCHAR* argv[])
+static PhysXSampleApplication* gApp = NULL;
+static SampleSetup gSettings;
+static SampleCommandLine* gSampleCommandLine = NULL;
+
+#define SEPARATE_EVENT_LOOP 1
+
+void mainInitialize()
 {
+	PX_ASSERT(gSampleCommandLine);
+	const SampleCommandLine& cmdline = *gSampleCommandLine;
+	initSampleAllocator();
+	gApp = SAMPLE_NEW(PhysXSampleApplication)(cmdline);
+
+	gApp->customizeSample(gSettings);
+
+	if (gApp->isOpen())
+		gApp->close();
+
+	gApp->open(gSettings.mWidth, gSettings.mHeight, gSettings.mName, gSettings.mFullscreen);
+
+#if SEPARATE_EVENT_LOOP
+	gApp->start(Ps::Thread::getDefaultStackSize());
+#else
+	if(gApp->isOpen()) gApp->onOpen();
+#endif
+}
+
+void mainTerminate()
+{
+	DELETESINGLE(gApp);
+	DELETESINGLE(gSampleCommandLine);
+	releaseSampleAllocator();
+}
+
+
+bool mainContinue()
+{
+	if (gApp->isOpen() && !gApp->isCloseRequested())
+	{
+		if(gApp->getInputMutex().trylock())
+		{
+			gApp->handleMouseVisualization();
+			gApp->doInput();
+			gApp->update();
+#if !SEPARATE_EVENT_LOOP
+			gApp->updateEngine();
+#endif
+			gApp->getInputMutex().unlock();
+		}
+		Thread::yield();
+		return true;
+	}
+
+#if SEPARATE_EVENT_LOOP
+	gApp->signalQuit();
+	gApp->waitForQuit();
+#else
+	if (gApp->isOpen() || gApp->isCloseRequested())
+		gApp->close();
+#endif
+
+	return false;
+}
+
+void mainLoop()
+{
+	while(mainContinue());
+}
+
+int main()
+{
+	gSampleCommandLine = new SampleCommandLine(GetCommandLineA());
+	mainInitialize();
+	mainLoop();
+	mainTerminate();
 	return 0;
 }
 
