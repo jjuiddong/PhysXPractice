4969ba9f3129686889292836f156ba0c514c9346
jjuiddong <jjuiddong@gmail.com>
2013-11-10 16:04:27 +0900

    update practice project

---
+++ Practice/main.cpp
@@ -0,0 +1,19 @@
+// Practice.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+#include "foundation/PxAssert.h"
+#include "RendererConfig.h"
+#include "SampleCommandLine.h"
+#include "RendererMemoryMacros.h"
+#include "SampleAllocator.h"
+#include "PhysXSampleApplication.h"
+#include "PsFile.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
